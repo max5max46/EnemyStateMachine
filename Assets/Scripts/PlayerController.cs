daa9295a2debb8911c581e2b46bfab7a9b12ec9b
Dylan Adams <113365542+max5max46@users.noreply.github.com>
2023-03-10 14:06:37 -0400

    added sliding

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,6 +24,7 @@ public class PlayerController : MonoBehaviour
     float moveLeftRight;
     float moveForwardBackward;
     float noise;
+    float timeStamp;
 
     bool upPressed;
     bool downPressed;
@@ -32,6 +33,8 @@ public class PlayerController : MonoBehaviour
     bool sprintPressed;
     bool jumpPressed;
     bool crouchPressed;
+
+    bool disableRegularForce;
 
 
     // Start is called before the first frame update
@@ -49,6 +52,8 @@ public class PlayerController : MonoBehaviour
 
         noise = 0.5f;
 
+        
+
         upPressed = false;
         downPressed = false;
         rightPressed = false;
@@ -56,6 +61,8 @@ public class PlayerController : MonoBehaviour
         sprintPressed = false;
         jumpPressed = false;
         crouchPressed = false;
+
+        disableRegularForce = false;
     }
 
 
@@ -80,7 +87,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
             jumpPressed = true;
 
-        if (Input.GetKey(KeyCode.LeftControl))
+        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.C))
             crouchPressed = true;
     }
 
@@ -90,35 +97,75 @@ public class PlayerController : MonoBehaviour
         //set noise to default value (walking)
         noise = 0.7f;
 
+        //resets drag/gravity incase anything changed them
+        rb.drag = 1;
+        rb.angularDrag = 0.05f;
+        rb.useGravity = true;
+
+        disableRegularForce = false;
+        
         //set acceleration and max speed back to normal after sprint
         acceleration = accelerationBaseValue;
         maxSpeed = maxSpeedBaseValue;
 
-        //If sprint is input up the accelertion and speed as well as change fov
+        //If sprint is input, up both accelertion and speed as well as change fov
         if (sprintPressed && !crouchPressed && (upPressed || leftPressed || rightPressed))
         {
             sprintPressed = false;
             noise = 1f;
             maxSpeed = maxSpeed * sprintMultiplier;
             acceleration = acceleration * sprintMultiplier;
-            PlayerCameraComponent.fieldOfView = Mathf.Lerp(PlayerCameraComponent.fieldOfView, 55, Time.deltaTime * 10);
+            //PlayerCameraComponent.fieldOfView = Mathf.Lerp(PlayerCameraComponent.fieldOfView, 70, Time.deltaTime * 2);
         }
         else
-            PlayerCameraComponent.fieldOfView = Mathf.Lerp(PlayerCameraComponent.fieldOfView, 60, Time.deltaTime * 10);
+            //PlayerCameraComponent.fieldOfView = Mathf.Lerp(PlayerCameraComponent.fieldOfView, 60, Time.deltaTime * 10);
 
-        //If crouch is input lower the camera and Shorten the Collider of the player
-        if (isGrounded && crouchPressed && !sprintPressed)
+        //If crouch is input, lower the camera and Shorten the Collider of the player
+        if (isGrounded && crouchPressed)
         {
             crouchPressed = false;
-            noise = 0.2f;
-            maxSpeed = maxSpeed * crouchMultiplier;
-            acceleration = acceleration * crouchMultiplier;
-            playerCamera.transform.position = new Vector3(playerCamera.transform.position.x, Mathf.Lerp(playerCamera.transform.position.y , transform.position.y + 0.2f, Time.deltaTime * 10), playerCamera.transform.position.z);
+
+            //reduces player and camera height
+            playerCamera.transform.position = new Vector3(playerCamera.transform.position.x, Mathf.Lerp(playerCamera.transform.position.y, transform.position.y + 0.2f, Time.deltaTime * 10), playerCamera.transform.position.z);
             transform.GetComponent<CapsuleCollider>().height = 1;
-            transform.GetComponent<CapsuleCollider>().center = new Vector3 (0, -0.5f, 0);
+            transform.GetComponent<CapsuleCollider>().center = new Vector3(0, -0.5f, 0);
+
+            if (Math.Sqrt(Math.Pow(rb.velocity.x, 2) + Math.Pow(rb.velocity.z, 2)) > 4 && upPressed)
+            {
+                if (timeStamp == 0)
+                    timeStamp = Time.time;
+
+                Debug.Log(timeStamp);
+
+                noise = 0.4f;
+
+                //disable any deceleration
+                disableRegularForce = true;
+
+                if (timeStamp + 0.7 > Time.time)
+                {
+                    rb.drag = 0;
+                    rb.angularDrag = 0;
+                    rb.useGravity = false;
+                }
+
+                maxSpeed = maxSpeed * sprintMultiplier * 1.2f;
+
+                if (Math.Sqrt(Math.Pow(rb.velocity.x, 2) + Math.Pow(rb.velocity.z, 2)) < maxSpeed || timeStamp + 0.7 > Time.time)
+                    rb.AddForce(rb.velocity);
+
+            }
+            else
+            {
+                noise = 0.2f;
+                maxSpeed = maxSpeed * crouchMultiplier;
+                acceleration = acceleration * crouchMultiplier;
+            }
+
         }
         else
         {
+            timeStamp = 0;
             playerCamera.transform.position = new Vector3(playerCamera.transform.position.x, Mathf.Lerp(playerCamera.transform.position.y, transform.position.y + 0.6f, Time.deltaTime * 10), playerCamera.transform.position.z);
             transform.GetComponent<CapsuleCollider>().height = 2;
             transform.GetComponent<CapsuleCollider>().center = Vector3.zero;
@@ -157,13 +204,17 @@ public class PlayerController : MonoBehaviour
             leftPressed = false;
         }
 
-        //Uses the Add force Variables to Add force as well as capping the speed
-        if (Math.Sqrt(Math.Pow(rb.velocity.x, 2) + Math.Pow(rb.velocity.z, 2)) < maxSpeed)
-            rb.AddRelativeForce(new Vector3(moveLeftRight, 0, moveForwardBackward).normalized * Time.deltaTime * 1000 * acceleration);
+        if (!disableRegularForce)
+        {
+            
+            //Uses the Add force Variables to Add force as well as capping the speed
+            if (Math.Sqrt(Math.Pow(rb.velocity.x, 2) + Math.Pow(rb.velocity.z, 2)) < maxSpeed)
+                rb.AddRelativeForce(new Vector3(moveLeftRight, 0, moveForwardBackward).normalized * Time.deltaTime * 1000 * acceleration);
 
+            //Increases deceleration to prevent sliding
+            rb.velocity = new Vector3 (rb.velocity.x * 0.9f, rb.velocity.y, rb.velocity.z * 0.9f);
+        }
 
-        //Increases deceleration to prevent sliding
-        rb.velocity = new Vector3 (rb.velocity.x * 0.9f, rb.velocity.y, rb.velocity.z * 0.9f);
 
         //if the player is on the ground and pressed jump then add force to the y for a jump
         if (jumpPressed && isGrounded)
@@ -174,6 +225,11 @@ public class PlayerController : MonoBehaviour
         //if standing still make "no noise"
         if ((rb.velocity.x < 0.1f && rb.velocity.x > -0.1f) && (rb.velocity.z < 0.1f && rb.velocity.z > -0.1f) || !isGrounded)
             noise = 0.05f;
+
+        Debug.Log(Mathf.Clamp01(((float)Math.Sqrt(Math.Pow(rb.velocity.x, 2) + Math.Pow(rb.velocity.z, 2)) - 3) / ((maxSpeedBaseValue * sprintMultiplier * 1.2f) - 3)));
+
+        PlayerCameraComponent.fieldOfView = Mathf.Lerp(PlayerCameraComponent.fieldOfView, 60 + (15 * Mathf.Clamp01(((float) Math.Sqrt(Math.Pow(rb.velocity.x, 2) + Math.Pow(rb.velocity.z, 2)) - 2) / ((maxSpeedBaseValue * sprintMultiplier * 1.2f) - 2))), Time.deltaTime * 2);
+    
 
         //DEBUG: check total x y speed
         //Debug.Log(Math.Sqrt(Math.Pow(rb.velocity.x, 2) + Math.Pow(rb.velocity.z, 2)));
